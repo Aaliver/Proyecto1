27bec84260e91da3f85a992f7f0146207f4b20c9
Valeria Becerril <aaliver@ciencias.unam.mx>
2026-09-10 18:14:40 -0600

    build: change executable names

---
+++ src/main/client/cliente.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Cliente!" << std::endl;
+    return 0;
+}
---
+++ src/main/server/servidor.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Servidor!" << std::endl;
+    return 0;
+}
